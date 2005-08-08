321ca6f0a28275e9e8064a0f72dc01a81b61c59b
liyuan-rey <10598397+liyuan-rey@users.noreply.github.com>
2005-08-08 08:32:37 +0000

    change folder structure complete!
    git-svn-id: svn://localhost/Tima/trunk@71 b770d3a9-0217-8245-951e-f2c8027e638c

--- code/Tima/skin/SkinDlgBgHelper.h
+++ code/Tima/skin/SkinDlgBgHelper.h
@@ -3,6 +3,50 @@
 class CSkinDlgBgHelper
 {
 public:
-	CSkinDlgBgHelper(void);
-	~CSkinDlgBgHelper(void);
+	afx_msg BOOL OnEraseBkgnd(CDC* pDC) {
+		CRect rcClip;
+		pDC->GetClipBox(rcClip);
+		FillRect(pDC->m_hDC, rcClip, GetBgBrush());
+
+		return TRUE;
+	}
+
+	afx_msg HBRUSH OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor, HBRUSH hbr) {
+
+		switch (nCtlColor)
+		{
+		case CTLCOLOR_STATIC:
+			{
+				pDC->SetBkMode(TRANSPARENT);
+				hbr = GetBgBrush();
+
+				break;
+			}
+		case CTLCOLOR_MSGBOX:
+		case CTLCOLOR_EDIT:
+		case CTLCOLOR_LISTBOX:
+		case CTLCOLOR_BTN:
+		case CTLCOLOR_DLG:
+		case CTLCOLOR_SCROLLBAR:
+			break;
+		default:
+			ATLASSERT(0);
+			break;
+		}
+
+		return hbr;
+	}
+
+private:
+	HBRUSH GetBgBrush() {
+		if (!m_bgBrush.GetSafeHandle())
+		{
+			COLORREF bgclr = SkinManager::Instance().GetCurrentSkin()->GetWindowBgColor();
+			m_bgBrush.CreateSolidBrush(bgclr);
+		}
+
+		return m_bgBrush;
+	}
+
+	CBrush m_bgBrush;
 };
